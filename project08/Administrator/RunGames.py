6acd5560db9893dae5b77bf1fee052f29f9948cf
Sudeepthi <sudeepthirebbalapalli@Sudeepthis-MacBook-Air.local>
2025-05-28 23:10:43 -0400

    Acquire

--- project08/Administrator/RunGames.py
+++
@@ -1,7 +0,0 @@
-from AutomatedGamePlay import AutomatedGamePlay
-
-for i in range(0,100):
-    print("Game "+str(i+1))
-    g=AutomatedGamePlay()
-    g.playGame()
-    print("-------------------------GAME END------------------------------")
